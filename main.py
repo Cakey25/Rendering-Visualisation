54037a6864585a71adc263ce9ed9afff44091d46
Cakey25 <cakey25yt@gmail.com>
2025-07-21 14:40:54 +0100

    testing properties, will start thing now

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from dataclasses import dataclass
 class Test:
     name: str
 
+    @property
+    def double(self):
+        return self.name + self.name
+
 def main():
 
     test = Test('word')
     print(test.name)
+
+    print(test.double)
 
 main()
