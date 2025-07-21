9c04242cf83594dc6a4a4bbbbed69b2a8eb32cb6
Cakey25 <cakey25yt@gmail.com>
2025-07-21 15:55:13 +0100

    this is the version of validating things i will go with for now

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from camera import Camera
 from window import Display, create_display
 
 from vectors import Vector3, Vector2
-from dataclasses import fields
 '''
+from dataclasses import dataclass
+
 @dataclass
 class Test:
     name: str
@@ -51,10 +52,10 @@ def clamp_int(num: int, lower: int, upper: int) -> int:
 def validate_colour(col: object):
     def wrapper(*args, **kwargs):
         colour = col(*args, **kwargs)
-        for field in fields(col):
-            print(field)
-            value = getattr(col, field.name)
-            setattr(col, field.name, clamp_int(num=value, lower=0, upper=255))
+        colour.r = clamp_int(colour.r, 0, 255)
+        colour.g = clamp_int(colour.g, 0, 255)
+        colour.b = clamp_int(colour.b, 0, 255)
+        colour.a = clamp_int(colour.a, 0, 255)
         return colour
     return wrapper
 
@@ -73,4 +74,5 @@ col = Colour(
 )
 
 print(col.r, col.g, col.b)
+
 #main()
