a73cc352b0d2db464f711c0b18f7e7cfa7a663e5
Cakey25 <cakey25yt@gmail.com>
2025-07-22 11:41:39 +0100

    forgot this one

--- matrices.py
+++ matrices.py
@@ -44,7 +44,7 @@ def matrix4_dot_vector4(matrix: Matrix4, vector: Vector3) -> Vector2 | bool:
     result.y = matrix[4]  * vec4.x + matrix[5]  * vec4.y + matrix[6]  * vec4.z + matrix[7]  * vec4.w
     result.z = matrix[8]  * vec4.x + matrix[9]  * vec4.y + matrix[10] * vec4.z + matrix[11] * vec4.w
     result.w = matrix[12] * vec4.x + matrix[13] * vec4.y + matrix[14] * vec4.z + matrix[15] * vec4.w
-    print(result)
+
     if result.w > 0.01: 
         result.x = result.x / result.w
         result.y = result.y / result.w
